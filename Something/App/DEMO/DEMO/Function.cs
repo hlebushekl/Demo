80f068c2b48adb65b7fd7ac2542f1c1b8c68d457
Arefev <127677998+hlebushekl@users.noreply.github.com>
2024-10-08 18:20:16 +0300

    Add files via upload

---
+++ Something/App/DEMO/DEMO/Function.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.OleDb;
+using System.Security.Cryptography;
+
+namespace DEMO
+{
+    public class DataReader
+    {
+        public static OleDbConnection connection
+        {
+            get { return new OleDbConnection(""); }
+        }
+    }
+
+    public class DataChenger
+    {
+        public static DataTable Data()
+        {
+            string NameTable = null;
+            OleDbConnection connection = DataReader.connection;
+
+            connection.Open();
+            OleDbDataAdapter adapter = new OleDbDataAdapter();
+            DataSet data = new DataSet();
+            adapter.Fill(data);
+            connection.Close();
+
+            return data.Tables[0];
+        }
+    }
+
+    public class Registration
+    {
+
+    }
+}
---
+++ Something/App/DEMO/Functions.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+public class DataReader
+{
+	public static 
+	{
+
+	}
+}
